2e967b98d7d7a78bd45560af3582df12b465ca05
Ordin <18818+lukelove@users.noreply.github.com>
2021-11-09 12:45:20 +0800

    reorganize, +zsh, chruby sourcing

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ echo "Alright, awesome!  Let's get this Mac Rocking!"
 echo "Installing the basics: 1Password, Spotify, Chrome and Spectacle 🎧"
 echo "✨✨✨"
 brew install --cask spectacle
-open -a Spectacle.app
+open -a Spectacle
 brew install --cask 1password
 open -a 1Password\ 7
 brew install --cask google-chrome
@@ -34,6 +34,15 @@ open -a Spotify
 echo "✨✨✨"
 echo "Installing everthing else.  This is going to take awhile!"
 echo "✨✨✨"
+
+# Communication
+brew install --cask slack
+brew install --cask whatsapp
+brew install --cask telegram
+brew install --cask discord
+brew install --cask loom
+brew install --cask signal
+
 # OS Basics
 brew install --cask firefox
 brew install --cask notion
@@ -43,6 +52,7 @@ brew install --cask the-unarchiver
 brew install --cask raindropio
 
 # Developer
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" # ZSH
 brew install --cask billings-pro
 brew install --cask iterm2
 brew install --cask visual-studio-code
@@ -55,6 +65,14 @@ brew install redis
 brew install --cask navicat-premium-essentials
 brew install ruby-install
 brew install chruby
+touch ~/.bash_profile
+touch ~/.zshrc
+echo "source /usr/local/opt/chruby/share/chruby/chruby.sh" >> ~/.bash_profile
+echo "source /usr/local/opt/chruby/share/chruby/auto.sh" >> ~/.bash_profile
+echo "source /usr/local/opt/chruby/share/chruby/chruby.sh" >> ~/.zshrc
+echo "source /usr/local/opt/chruby/share/chruby/auto.sh" >> ~/.zshrc
+
+
 brew install node
 brew install yarn
 brew install gawk
@@ -65,15 +83,6 @@ gem install rails
 ruby-install ruby 2.7.3
 gem install rails
 
-# Communication
-brew install --cask slack
-brew install --cask whatsapp
-brew install --cask telegram
-brew install --cask discord
-brew install --cask zoom
-brew install --cask loom
-brew install --cask signal
-
 # Media
 brew install --cask vlc
 brew install --cask audacity
@@ -83,6 +92,10 @@ brew install --cask figma
 #Crypto
 brew install --cask binance
 brew install --cask tradingview
+
+# At the bottom because it asks for sudo password
+brew install --cask zoom
+
 
 echo ""
 echo ""
